19be2c405fd5940be22a45923c96ce04ccf20a3c
waukan <uni@waukan.sakura.ne.jp>
2024-07-19 05:38:01 +0900

    Lesson15課題です。

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,14 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InputController {
+
+    @GetMapping("/input")
+    public String getInput() {
+        return "input";
+    }
+
+}
